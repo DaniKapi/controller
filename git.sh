8565e258b00c554f02eef88c570c94122baf5314
dallanosm@alumnos.unex.es <salabeta@salabeta9>
2015-10-27 16:24:29 +0100

    primera subida controler

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# controller" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/DaniKapi/controller.git
+git push -u origin master
