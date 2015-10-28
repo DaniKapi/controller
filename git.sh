d620fccfa0150de75d9337c8fbe937280f073264
DaniKapi <danielalbertokapi@gmail.com>
2015-10-28 18:00:09 +0100

    Delete git.sh

--- git.sh
+++
@@ -1,6 +0,0 @@
-echo "# controller" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/DaniKapi/controller.git
-git push -u origin master
